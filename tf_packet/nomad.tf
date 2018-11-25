deab54a6323ff774a8c902237f31c397948572ce
Alvaro Miranda Aguilera <kikitux@gmail.com>
2018-11-25 21:53:07 +0100

    update

--- tf_packet/nomad.tf
+++ tf_packet/nomad.tf
@@ -5,22 +5,30 @@ provider "nomad" {
 }
 
 resource "nomad_job" "hello-world" {
-  jobspec =  "${file("${path.module}/../nomad_jobs/hello-world.hcl")}"
+  jobspec = "${file("${path.module}/../nomad_jobs/hello-world.hcl")}"
 }
 
 resource "nomad_job" "bye-world" {
-  jobspec =  "${file("${path.module}/../nomad_jobs/bye-world.hcl")}"
+  jobspec = "${file("${path.module}/../nomad_jobs/bye-world.hcl")}"
+}
+
+resource "random_pet" "mypet" {
+  keepers = {
+    timestamp = "${timestamp()}"
+  }
+  separator = " "
 }
 
 data "template_file" "greeting" {
-    template = "${file("${path.module}/../nomad_jobs/greeting.tpl")}"
-    vars {
-        greeting = "Hello from terraform!" 
-    }
+  template = "${file("${path.module}/../nomad_jobs/greeting.tpl")}"
+
+  vars {
+    greeting = "Hello from terraform!, my pet is called ${random_pet.mypet.id}"
+  }
 }
 
 resource "nomad_job" "greeting" {
-  jobspec =  "${data.template_file.greeting.rendered}"
+  jobspec = "${data.template_file.greeting.rendered}"
 }
 
 output "sample http-echo" {
