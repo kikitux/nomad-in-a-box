6f20f938e05d762e0c918c499cdba2223ab45329
Alvaro Miranda Aguilera <kikitux@gmail.com>
2018-11-26 22:23:53 +0100

    Update output.tf

--- tf_packet/output.tf
+++ tf_packet/output.tf
@@ -1,29 +1,13 @@
-output "info consul1" {
-  value = "${format("http://consul1.%s.xip.io", packet_device.vmonpacket.access_public_ipv4)}"
+output "info consul" {
+  value = "${format("http://consul.%s.xip.io", packet_device.vmonpacket.access_public_ipv4)}"
 }
 
-output "info consul2" {
-  value = "${format("http://consul2.%s.xip.io", packet_device.vmonpacket.access_public_ipv4)}"
+output "info vault" {
+  value = "${format("http://vault.%s.xip.io", packet_device.vmonpacket.access_public_ipv4)}"
 }
 
-output "info consul3" {
-  value = "${format("http://consul3.%s.xip.io", packet_device.vmonpacket.access_public_ipv4)}"
-}
-
-output "info vault1" {
-  value = "${format("http://vault1.%s.xip.io", packet_device.vmonpacket.access_public_ipv4)}"
-}
-
-output "info nomad1" {
-  value = "${format("http://nomad1.%s.xip.io", packet_device.vmonpacket.access_public_ipv4)}"
-}
-
-output "info nomad2" {
-  value = "${format("http://nomad2.%s.xip.io", packet_device.vmonpacket.access_public_ipv4)}"
-}
-
-output "info nomad3" {
-  value = "${format("http://nomad3.%s.xip.io", packet_device.vmonpacket.access_public_ipv4)}"
+output "info nomad" {
+  value = "${format("http://nomad.%s.xip.io", packet_device.vmonpacket.access_public_ipv4)}"
 }
 
 output "public_ip" {
