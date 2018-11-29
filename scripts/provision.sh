512366b4fe240146a41630c67fcf0453fdd607fd
Alvaro Miranda Aguilera <kikitux@gmail.com>
2018-11-29 20:36:32 +0100

    refactor

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
+## BEGIN of customization
+
 # versions
 CONSUL=1.4.0
+CONSUL_TEMPLATE=0.19.5
 VAULT=0.11.5
 NOMAD=0.8.6
 HTTPECHO=0.2.3
 
+## END of customization
+
 # if we are in a vagrant box, lets cd into /vagrant
 [ -d /vagrant ] && pushd /vagrant
 
-# check lxd, if not present
 # install and configure lxd
 which lxd &>/dev/null || {
   export DEBIAN_FRONTEND=noninteractive
@@ -27,9 +31,7 @@ which lxd &>/dev/null || {
 lxc info base &>/dev/null || {
   lxc launch ubuntu:16.04 base -c security.nesting=true
   echo sleeping so base get an IP
-  sleep 10
-  lxc list
-    # create dir and copy server.hcl for consul
+  sleep 8
   mkdir -p /var/lib/lxd/containers/base/rootfs/etc/dpkg/dpkg.cfg.d/
   cp conf/01_nodoc /var/lib/lxd/containers/base/rootfs/etc/dpkg/dpkg.cfg.d/01_nodoc
   lxc exec base -- apt-get update
@@ -56,9 +58,25 @@ lxc info base &>/dev/null || {
 # copy scripts to all existing nodes
 for d in /var/lib/lxd/containers/*/rootfs/var/tmp; do
   cp scripts/consul.sh ${d}
-  cp scripts/vault.sh ${d}
   cp scripts/nomad.sh ${d}
+  cp scripts/vault.sh ${d}
 done
+
+# base-client
+s=base-client
+lxc info ${s} &>/dev/null || {
+  echo "copying base into ${s}"
+  lxc copy base ${s}
+  lxc start ${s}
+  echo sleeping so ${s} get an IP
+  sleep 8
+  lxc exec ${s} -- apt-get update
+  lxc exec ${s} -- apt-get install --no-install-recommends -y docker.io
+  lxc exec ${s} -- apt-get install --no-install-recommends -y default-jre
+  lxc exec ${s} -- apt-get clean
+  lxc exec ${s} -- docker run hello-world &>/dev/null && echo docker hello-world works
+  lxc stop base-client
+} & #background
 
 # create consul
 for s in consul{1..3}; do
@@ -74,8 +92,7 @@ for s in consul{1..3}; do
     cp conf/consul.d/server.hcl /var/lib/lxd/containers/${s}/rootfs/etc/consul.d
     cp conf/consul.service /var/lib/lxd/containers/${s}/rootfs/etc/systemd/system
     lxc exec ${s} -- bash /var/tmp/consul.sh
-  }
-
+  } & # background
 done
 
 consul_client(){
@@ -102,7 +119,7 @@ lxc info ${s} &>/dev/null || {
   cp conf/vault.d/server.hcl /var/lib/lxd/containers/${s}/rootfs/etc/vault.d
   cp conf/vault.service /var/lib/lxd/containers/${s}/rootfs/etc/systemd/system
   lxc exec ${s} -- bash /var/tmp/vault.sh
-}
+} & # background
 
 # create nomad
 for s in nomad{1..3}; do
@@ -121,8 +138,7 @@ for s in nomad{1..3}; do
     cp conf/nomad.service /var/lib/lxd/containers/${s}/rootfs/etc/systemd/system
 
     lxc exec ${s} -- bash /var/tmp/nomad.sh
-  }
-
+  } & # background
 done
 
 # install packages needed on the host
@@ -133,7 +149,6 @@ which haproxy nginx unzip wget &>/dev/null || {
 
 # configure nginx to expose services
 [ -f /etc/nginx/sites-enabled/default ] && rm /etc/nginx/sites-enabled/default
-#cp conf/nginx.conf /etc/nginx/sites-enabled/default
 service nginx restart
 
 # configure haproxy to expose ui
@@ -141,7 +156,7 @@ cp conf/haproxy.cfg /etc/haproxy/haproxy.cfg
 service haproxy restart
 
 [ -f /usr/local/bin/consul-template ] || {
-  wget -O /tmp/consul-template.zip https://releases.hashicorp.com/consul-template/0.19.5/consul-template_0.19.5_linux_amd64.zip
+  wget -O /tmp/consul-template.zip https://releases.hashicorp.com/consul-template/${CONSUL_TEMPLATE}/consul-template_${CONSUL_TEMPLATE}_linux_amd64.zip
   unzip -d /usr/local/bin /tmp/consul-template.zip
 }
 
@@ -150,22 +165,7 @@ cp conf/consul-template.service /etc/systemd/system/consul-template.service
 systemctl enable consul-template.service
 systemctl restart consul-template.service
 
-# base-client
-s=base-client
-lxc info ${s} &>/dev/null || {
-  echo "copying base into ${s}"
-  lxc copy base ${s}
-  lxc start ${s}
-  echo sleeping so ${s} get an IP
-  sleep 8
-  lxc list ${s}
-  lxc exec ${s} -- apt-get update
-  lxc exec ${s} -- apt-get install --no-install-recommends -y docker.io
-  lxc exec ${s} -- apt-get install --no-install-recommends -y default-jre
-  lxc exec ${s} -- apt-get clean
-  lxc exec ${s} -- docker run hello-world &>/dev/null && echo docker hello-world works
-  lxc stop base-client
-}
+wait  # wait for background processes to finish
 
 # clients
 for s in client{1..3}; do
@@ -175,7 +175,6 @@ for s in client{1..3}; do
     lxc start ${s}
     echo sleeping so ${s} get an IP
     sleep 8
-    lxc list ${s}
 
     consul_client
        
@@ -185,6 +184,6 @@ for s in client{1..3}; do
     cp conf/nomad.service /var/lib/lxd/containers/${s}/rootfs/etc/systemd/system
 
     lxc exec ${s} -- bash /var/tmp/nomad.sh
-  }
-  
- done
+  } & # background
+done
+wait  # wait for background processes to finish
