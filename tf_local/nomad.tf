e8d26f6844c0025094cd49d2cf98555609c88007
Alvaro Miranda Aguilera <kikitux@gmail.com>
2018-12-02 13:29:51 +0100

    Update nomad.tf

--- tf_local/nomad.tf
+++ tf_local/nomad.tf
@@ -4,34 +4,10 @@ provider "nomad" {
   region  = "global"
 }
 
-resource "nomad_job" "hello-world" {
-  jobspec = "${file("${path.module}/../nomad_jobs/hello-world.hcl")}"
+resource "nomad_job" "nginx" {
+  jobspec = "${file("${path.module}/../nomad_jobs/nginx.hcl")}"
 }
 
-resource "nomad_job" "bye-world" {
-  jobspec = "${file("${path.module}/../nomad_jobs/bye-world.hcl")}"
-}
-
-resource "random_pet" "mypet" {
-  keepers = {
-    timestamp = "${timestamp()}"
-  }
-
-  separator = " "
-}
-
-data "template_file" "greeting" {
-  template = "${file("${path.module}/../nomad_jobs/greeting.tpl")}"
-
-  vars {
-    greeting = "Hello from terraform!, my pet name is ${random_pet.mypet.id}"
-  }
-}
-
-resource "nomad_job" "greeting" {
-  jobspec = "${data.template_file.greeting.rendered}"
-}
-
-output "sample http-echo" {
-  value = "http://http-echo.127.0.0.1.xip.io:8080"
+output "sample nginx" {
+  value = "http://nginx.127.0.0.1.xip.io:8080"
 }
